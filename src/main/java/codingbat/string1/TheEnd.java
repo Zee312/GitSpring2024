cd35271ede4c4e013188c0d630778feb1cf9e1ef
Zarina <andarbekovazarina@gmail.com>
2024-08-12 01:39:10 +0600

    String1/TheEnd

---
+++ src/main/java/codingbat/string1/TheEnd.java
@@ -0,0 +1,9 @@
+package codingbat.string1;
+
+public class TheEnd {
+    public String theEnd(String str, boolean front) {
+        if(front == true){
+            return str.substring (0,1);
+        }return str.substring (str.length()-1);
+    }
+}
