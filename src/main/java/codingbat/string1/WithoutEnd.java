800617efb55ad66433ace8f2c780918bf6d8787b
Zarina <andarbekovazarina@gmail.com>
2024-08-12 01:46:45 +0600

    String1/WithoutEnd

---
+++ src/main/java/codingbat/string1/WithoutEnd.java
@@ -0,0 +1,9 @@
+package codingbat.string1;
+
+public class WithoutEnd {
+    public String withoutEnd(String str) {
+        String str2 = str.substring (1,str.length()-1);
+        return str2;
+    }
+
+}
